8c5c4fd1c5543b3b85956758d5e2de4276fcf246
Bello Khadijah Omobolanle <abdulsalamkhadijahh@gmail.com>
2020-10-08 15:53:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿let user={
+    name:"kdj",
+    age:20,
+    sizes:{
+        height:200,
+        width: 100
+    }
+};
+let clone=Object.assign({}, user);
+  clone.sizes.width===user.sizes.width;
+
+  console.log(clone.name);
+    
